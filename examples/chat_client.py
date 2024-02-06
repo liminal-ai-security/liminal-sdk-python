763856d05a4e3e87e654f9a94cd9733d75f81f82
Aaron Bach <bachya1208@gmail.com>
2024-02-06 10:46:06 -0700

    Add a chat client example (#12)
    * first commit for AB
    
    * Base structure
    
    * Can authenticate!
    
    * Add ability to get available LLMs
    
    * Better typing
    
    * Add source to every call
    
    * Add ability to get thread and create a thread
    
    * Added ability to get thread by ID
    
    * Add ability to delete a thread
    
    * Add endpoint to analyze a prompt
    
    * Update example
    
    * Remove thread deletion for now (doesn't cascade properly)
    
    * Add ability to cleanse a prompt
    
    * Allow the use of previous findings when cleansing
    
    * Add endpoint for getting deidentified context history
    
    * Add ability to include deidentified history in prompt cleansing
    
    * Submit a prompt and get a response
    
    * Add manual and automatic refresh tokens
    
    * Add ability to specify refresh token callbacks
    
    * More work
    
    * Fix
    
    * Reset token refresh flag
    
    * Good docs
    
    * Add GH stuff
    
    * Add dependabot
    
    * Forget CodeQL for now
    
    * Add a chat client example
    
    * Fix GH Action
    
    * Bump line similarity
    
    ---------
    
    Co-authored-by: Gleb A <grizzly.gleb@gmail.com>

---
+++ examples/chat_client.py
@@ -0,0 +1,65 @@
+"""Define a chat client example."""
+import asyncio
+import logging
+import os
+
+from liminal import Client
+from liminal.endpoints.auth import MicrosoftAuthProvider
+from liminal.errors import LiminalError
+
+_LOGGER = logging.getLogger("example")
+
+LIMINAL_API_SERVER_URL = os.environ["LIMINAL_API_SERVER_URL"]
+CLIENT_ID = os.environ["CLIENT_ID"]
+TENANT_ID = os.environ["TENANT_ID"]
+
+
+async def main() -> None:
+    """Create the aiohttp session and run the example."""
+    logging.basicConfig(level=logging.INFO)
+
+    # Create an auth provider to authenticate the user:
+    microsoft_auth_provider = MicrosoftAuthProvider(TENANT_ID, CLIENT_ID)
+
+    # Create the liminal SDK instance:
+    liminal = Client(microsoft_auth_provider, LIMINAL_API_SERVER_URL)
+
+    try:
+        # Authenticate the user:
+        await liminal.authenticate_from_auth_provider()
+    except LiminalError as err:
+        _LOGGER.error("Error authenticating: %s", err)
+
+    try:
+        while True:
+            if (prompt := input("Enter a message: ")) == "quit":
+                break
+
+            created_thread = await liminal.thread.create("openai_35", "Demo Thread")
+
+            findings = await liminal.prompt.analyze(created_thread.id, prompt)
+            _LOGGER.info("Analysis findings: %s", findings)
+            _LOGGER.info("")
+
+            cleansed_prompt = await liminal.prompt.cleanse(
+                created_thread.id, prompt, findings=findings
+            )
+            _LOGGER.info("Cleansed prompt: %s", cleansed_prompt)
+            _LOGGER.info("")
+
+            deidentified_context_history = (
+                await liminal.thread.get_deidentified_context_history(created_thread.id)
+            )
+
+            response = await liminal.prompt.submit(
+                created_thread.id,
+                prompt,
+                findings=findings,
+                deidentified_context_history=deidentified_context_history,
+            )
+            _LOGGER.info("LLM response: %s", response)
+    except LiminalError as err:
+        _LOGGER.error("Error while running example: %s", err)
+
+
+asyncio.run(main())
