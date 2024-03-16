d4346a0afbe994a00c9b2534dcd8013b91652830
Aaron Bach <bachya1208@gmail.com>
2024-03-16 13:59:10 -0600

    ci: add several CI QOL updates (#49)

--- liminal/errors.py
+++ liminal/errors.py
@@ -4,16 +4,10 @@
 class LiminalError(Exception):
     """Define a base exception."""
 
-    pass
-
 
 class RequestError(LiminalError):
     """Define an exception related to an HTTP request."""
 
-    pass
-
 
 class AuthError(RequestError):
     """Define an exception related to invalid auth."""
-
-    pass
